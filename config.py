32e689a307d8bfe9aac2c28a4c57ed6411830650
Gage Whitt <gage@differential.com>
2019-04-06 01:18:15 -0400

    First Commit.

---
+++ config.py
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+SECRET_KEY='a unique and long key'
+TITLE='Riki' 
+HISTORY_SHOW_MAX=30
+PIC_BASE = '/static/content/'
+CONTENT_DIR = '/Users/gagewhitt/venv/p3/Wiki440/content'
+USER_DIR = '/Users/gagewhitt/venv/p3/Wiki440/user'
+NUMBER_OF_HISTORY = 5
+PRIVATE = True
